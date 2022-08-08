c8b33117024c3cde4d0fac1e27d01544630272e7
Ramzi Arfaoui <ramzi_arfa@hotmail.de>
2022-08-08 18:48:45 +0200

    initialise feature bundle

---
+++ README.md
@@ -0,0 +1,28 @@
+# Ibexa location fieldtype Bundle
+
+
+## Requirement
+
+Ibexa **4.x +**
+
+
+## Features
+
+## Limitations
+ 
+## Installation
+
+```bash
+composer require arfaram/ibexa-location-fieldtype
+```
+
+- Activate the Bundle in `bundles.php`
+
+```php
+    Ibexa\LocationFieldTypeBundle\IbexaLocationFieldTypeBundle::class => ['all' => true],
+```
+
+
+## Screenshots
+
+
---
+++ src/bundle/IbexaLocationFieldTypeBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Ibexa\LocationFieldTypeBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class IbexaLocationFieldTypeBundle extends Bundle
+{
+    /**
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+     */
+    public function build(ContainerBuilder $container)
+    {
+    }
+}
